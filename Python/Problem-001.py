bb15d99c670b91eeddd2914063053995594c82c3
Bill Dybas <wmdybas@gmail.com>
2015-08-09 15:18:12 -0400

    Tweaked some things.

--- Python/Problem-001.py
+++ Python/Problem-001.py
@@ -1,7 +1,16 @@
-multiples = []
+#
+# Solution to Problem 1
+# by Bill Dybas
+#
 
-for i in range(1000):
-	if i % 3 == 0 or i % 5 == 0:
-		multiples.append(i)
+def calculate():
+	multiples = []
 
-print(sum(multiples))
+	for i in range(1000):
+		if i % 3 == 0 or i % 5 == 0:
+			multiples.append(i)
+
+	print(sum(multiples))
+
+if __name__ == '__main__':
+	calculate()
