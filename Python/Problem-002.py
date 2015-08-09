9001e849108be1d7f86f382891efc4e7959e6a54
Bill Dybas <wmdybas@gmail.com>
2015-08-09 16:09:38 -0400

    Finished Problem 2

---
+++ Python/Problem-002.py
@@ -0,0 +1,19 @@
+#
+# Solution to Problem 2
+# by Bill Dybas
+#
+
+def calculate(limit):
+    sum = 0
+    fib_1, fib_2 = 1, 2
+
+    while fib_1 < limit:
+        if fib_1 % 2 == 0:
+            sum += fib_1
+
+        fib_1, fib_2 = fib_2, fib_1 + fib_2
+
+    return sum
+
+if __name__ == '__main__':
+    print(calculate(4000000))
